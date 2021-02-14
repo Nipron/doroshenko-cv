7be25a0936936778d878e4a29d8af7b95a9311f4
Vitalii Doroshenko <2328221@ukr.net>
2021-02-14 14:18:54 +0200

    test links

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,106 +1,92 @@
 import React, {useEffect, useState} from 'react';
 import "./Carousel.scss";
 import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.min.css'
-import 'owl.carousel/dist/assets/owl.theme.default.min.css'
-import s from "./AppCard.module.css"
-import ss from "./CarouselStyle.module.css"
-import boutique from "../images/AppThumbnails/boutique.jpg"
-import beerstore from "../images/AppThumbnails/beerstore.jpg"
+import 'owl.carousel/dist/assets/owl.carousel.min.css';
+import 'owl.carousel/dist/assets/owl.theme.default.min.css';
+import s from "./AppCard.module.css";
+import ss from "./CarouselStyle.module.css";
+import boutique from "../images/AppThumbnails/boutique.jpg";
+import beerstore from "../images/AppThumbnails/beerstore.jpg";
+import bootstrap from "../images/Logos/bootstrap.png";
+import styled from "../images/Logos/styled.png";
+import react from "../images/Logos/react.png";
+import paypal from "../images/Logos/paypal.png";
 
 const Carousel = () => {
 
     return (
-            <section className={ss.myApps}>
-                <OwlCarousel
-                    items="1"
-                    class="carousel owl-carousel"
-                    loop="true"
-                    center="true"
-                    //autoplay="true"
-                    autoWidth="true"
-                    /*autoplayTimeout="5000"*/>
+        <section className={ss.myApps}>
+            <OwlCarousel
+                items="1"
+                class="carousel owl-carousel"
+                loop="true"
+                center="true"
+                //autoplay="true"
+                autoWidth="true"
+                /*autoplayTimeout="5000"*/>
 
-                    <div className="item">
-                        <div className={s.wrap}>
-                            <div className={s.card}>
-                                <div className={s.heading}>
-                                    Simple E-commerce project
-                                </div>
-                                <div className={s.picture}>
-                                    <img src={beerstore} alt="beer store"/>
-                                </div>
-                                <div className={s.description}>
-                                    <div> One of my first projects - simple E-commerce page with hardcoded data.</div>
-                                    <div> Combination of Bootstrap 4 and Styled Components.</div>
-                                </div>
-                                <div className={s.icons}>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                </div>
+                <div className="item">
+                    <div className={s.wrap}>
+                        <div className={s.card}>
+                            <div className={s.heading}>
+                                Simple E-commerce project
+                            </div>
+                            <div className={s.picture}>
+                                <img src={boutique} alt="beer store"/>
+                            </div>
+                            <div className={s.description}>
+                                <div>One of my first projects - simple E-commerce project with hardcoded data.</div>
+                                <div>Combination of Bootstrap 4 and Styled Components.</div>
+                                <div>PayPal sandbox payments.</div>
+                            </div>
+                            <div className={s.icons}>
+
+                                <img className={s.iconZ} src={react} alt="bootstap"/>
+                                <img className={s.iconZ} src={bootstrap} alt="bootstap"/>
+                                <img className={s.iconZ} src={styled} alt="bootstap"/>
+                                <img className={s.iconZ} src={paypal} alt="bootstap"/>
+
+
+                                {/*<img className={s.icon} src={bootstrap}/>
+                                    <img className={s.icon} src={bootstrap}/>
+                                    <img className={s.icon} src={bootstrap}/>*/}
                             </div>
                         </div>
                     </div>
+                </div>
+
+                <div className="item">
+                    <div className={s.wrap}>
+                        <div className={s.card}>
+                            <div className={s.heading}>
+                                Simple E-commerce project
+                            </div>
+                            <div className={s.picture}>
+                                <img src={beerstore} alt="beer store"/>
+                            </div>
+                            <div className={s.description}>
+                                <div>One of my first projects - simple E-commerce page with hardcoded data.</div>
+                                <div>Combination of Bootstrap 4 and Styled Components.</div>
+                                <div>PayPal sandbox payments.</div>
+                            </div>
+                            <div className={s.icons}>
+
+                                <img className={s.iconZ} src={bootstrap} alt="bootstap"/>
+                                <img className={s.iconZ} src={styled} alt="bootstap"/>
 
 
-                    <div className="item">
-                        <div className={s.wrap}>
-                            <div className={s.card}>
-                                <div className={s.heading}>
-                                   E-commerce Project
-                                </div>
-                                <div className={s.pictureBox}>
-                                    <img className={s.picture} src={boutique} alt=""/>
-                                </div>
-                                <div className={s.description}>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                </div>
-                                <div className={s.icons}>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                </div>
+                                {/*<img className={s.icon} src={bootstrap}/>
+                                    <img className={s.icon} src={bootstrap}/>
+                                    <img className={s.icon} src={bootstrap}/>*/}
                             </div>
                         </div>
                     </div>
-
-                    <div className="item">
-                        <div className={s.wrap}>
-                            <div className={s.card}>
-                                <div className={s.heading}>
-                                    Lorem ipsum dolor
-                                </div>
-                                <div className={s.picture}>
-                                    <img src="#" alt=""/>
-                                </div>
-                                <div className={s.description}>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                </div>
-                                <div className={s.icons}>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="item">
-                        <div className={s.wrap}>
-                            <div className={s.card}>
-                                <div className={s.heading}>
-                                    Lorem ipsum dolor
-                                </div>
-                                <div className={s.picture}>
-                                    <img src="#" alt=""/>
-                                </div>
-                                <div className={s.description}>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                </div>
-                                <div className={s.icons}>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                </div>
 
 
-                </OwlCarousel>
+
+
+            </OwlCarousel>
         </section>
     );
 };
--- src/pages/CV/YouTube/YouTube.jsx
+++ src/pages/CV/YouTube/YouTube.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import s from "./YouTube.module.css";
-import EloquentJS from "../../../images/books/EloquentJS.jpg"
-import Grokaem from "../../../images/books/Grokaem.jpg"
-import LearningReact from "../../../images/books/LearningReact.jpg"
-import ReactHooks from "../../../images/books/ReactHooks.jpg"
+import {Link} from "react-router-dom";
+import Academind from "../../../images/YouTube/Academind.jpg";
+import Apollo from "../../../images/YouTube/Apollo.jpg";
+import Ashley from "../../../images/YouTube/Ashley.jpg";
+import Basir from "../../../images/YouTube/Basir.jpg";
+import Dimych from "../../../images/YouTube/Dimych.jpg";
+import FreeCodeCamp from "../../../images/YouTube/FreeCodeCamp.jpg";
+import Freelancer from "../../../images/YouTube/Freelancer.jpg";
+import John from "../../../images/YouTube/John.jpg";
+import LoftBlog from "../../../images/YouTube/LoftBlog.jpg";
+import Muhammad from "../../../images/YouTube/Muhammad.jpg";
+import Pasha from "../../../images/YouTube/Pasha.jpg";
+import Qazi from "../../../images/YouTube/Qazi.jpg";
+import Vladilen from "../../../images/YouTube/Vladilen.jpg";
+import Web from "../../../images/YouTube/Web.jpg";
+
 
 const YouTube = () => {
     return (
@@ -12,18 +24,46 @@ const YouTube = () => {
                 <h2>YouTube channels that I've spent a total of at least 1200 hours on</h2>
             </div>
             <div className={s.content}>
-                <div className={s.bookBox}>
-                    <img src={EloquentJS}/>
+                <div className={s.channelBox}>
+                    <img className={s.youtubeIcon} src={Dimych}/>
+                    <div>
+                        <a target="_blank"
+                           href="https://www.youtube.com/channel/UCTW0FUhT0m-Bqg2trTbSs0g">
+                            IT-KAMASUTRA
+                        </a>
+                    </div>
                 </div>
-                <div className={s.bookBox}>
-                    <img src={LearningReact}/>
+                <div className={s.channelBox}>
+                    <img className={s.youtubeIcon} src={Ashley}/>
+                    <div>
+                        efeffeef
+                    </div>
                 </div>
-                <div className={s.bookBox}>
-                    <img src={ReactHooks}/>
+                <div className={s.channelBox}>
+                    <img className={s.youtubeIcon} src={Dimych}/>
+                    <div>
+                        efeffeef
+                    </div>
                 </div>
-                <div className={s.bookBox}>
-                    <img src={Grokaem}/>
+                <div className={s.channelBox}>
+                    <img className={s.youtubeIcon} src={Dimych}/>
+                    <div>
+                        efeffeef
+                    </div>
                 </div>
+                <div className={s.channelBox}>
+                    <img className={s.youtubeIcon} src={Dimych}/>
+                    <div>
+                        efeffeef
+                    </div>
+                </div>
+                <div className={s.channelBox}>
+                    <img className={s.youtubeIcon} src={Dimych}/>
+                    <div>
+                        efeffeef
+                    </div>
+                </div>
+
             </div>
         </section>
     );
