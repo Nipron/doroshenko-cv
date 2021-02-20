f816263b09f8a05c54439f0d16c049cff5201c76
Vitalii Doroshenko <2328221@ukr.net>
2021-02-20 03:19:34 +0200

    zzz

--- src/components/Doroshenko/Doroshenko.jsx
+++ src/components/Doroshenko/Doroshenko.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import s from "./Doroshenko.module.css";
-import { HashLink as Link } from 'react-router-hash-link';
+import {HashLink as Link} from 'react-router-hash-link';
 
 const Doroshenko = () => {
     return (
         <div className={s.doroshenko}>
-                <Link to="/cv">VITALII <span>DOROSHENKO</span></Link>
+            <Link to="/cv">VITALII <span>DOROSHENKO</span></Link>
             <div className={s.text}>
                 HE'S BACK, AND HE'S
             </div>
             <div className={s.text}>
                 SMOKING', BABY!
+            </div>
+            <div className={s.ent}>
+                A React Enthusiast
             </div>
         </div>
     );
