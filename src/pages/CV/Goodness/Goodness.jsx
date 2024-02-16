25a6839dd96fe6c8c75d8ab350c76a57828bee5e
Vitalii Doroshenko <vitalii.doroshenko@meliorgames.com>
2024-02-16 09:56:07 +0200

    sc

--- src/pages/CV/Goodness/Goodness.jsx
+++ src/pages/CV/Goodness/Goodness.jsx
@@ -14,7 +14,8 @@ const Goodness = () => {
             <div className={s.content}>
                 <div className={s.contentBox + ' ' + s.w50}>
                     <h3>Goodness</h3>
-                    <p>{t("goodness1")} <a className={s.darkLink} target="_blank" href="https://lightcore.io/">LightCore</a> {t("goodness2")}</p>
+                    {/*<p>{t("goodness1")} <a className={s.darkLink} target="_blank" href="https://lightcore.io/">LightCore</a> {t("goodness2")}</p>*/}
+                    <p>{t("goodness1")} <a className={s.darkLink} target="_blank" href="ua.slotscity:///cashier">Slots City</a> {t("goodness2")}</p>
                 </div>
                 <div className={s.w50}>
                     <img src={goodness} className="img" alt="selfie"/>
